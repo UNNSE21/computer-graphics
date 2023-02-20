c04c219d362491c678d7478129e264e43a3fbf3e
Namxobick <yurinsin@gmail.com>
2023-02-20 17:34:38 +0300

    The blur filter has been optimized
    The extreme points are counted in the same way as all the others

--- filters-openCV/filters_opencv/filters/local_filters/arithmetic_mean/blur_optimized_by_andrey.py
+++ filters-openCV/filters_opencv/filters/local_filters/arithmetic_mean/blur_optimized_by_andrey.py
@@ -6,42 +6,62 @@ from filters_opencv.image import Image
 
 def blur(image: Image, radius_x: int, radius_y: int):
     copy_image = copy(image)
-    district_height = 2 * radius_y + 1
-    district_width = 2 * radius_x + 1
-    district_size = district_width * district_height
 
-    district = base_district = [x * district_size for x in _arithmetic_mean(
+    count_pixels = base_count_pixels = (radius_x + 1) * (radius_y + 1)
+    district = base_district = [x * count_pixels for x in _arithmetic_mean(
         image,
-        radius_x,
-        radius_y,
+        0,
+        0,
         radius_x,
         radius_y,
     )]
 
     for i in range(image.height):
         for j in range(image.width):
-            if i < radius_y or i + radius_y + 2 > image.height or j < radius_x or j + radius_x + 2 > image.width:
-                image[i, j] = _arithmetic_mean(copy_image, j, i, radius_x, radius_y)
-            else:
-                image[i, j] = [x / district_size for x in district]
-                district = _calculate_district(copy_image, district, j, i, radius_x, radius_y)
+            print(i, j)
+            image[i, j] = [x / count_pixels for x in district]
+            district, count_pixels = _calculate_district(copy_image, district, count_pixels, j, i, radius_x, radius_y)
 
-        if i >= radius_y and i + radius_y + 2 <= image.height:
-            base_district = _calculate_base_district(copy_image, base_district, i, district_width, radius_y)
-            district = base_district
+        base_district, base_count_pixels = _calculate_base_district(
+            copy_image,
+            base_district,
+            base_count_pixels,
+            i,
+            radius_x,
+            radius_y,
+        )
+
+        district = base_district
+        count_pixels = base_count_pixels
 
 
-def _calculate_district(image: Image, district, pos_x: int, pos_y: int, radius_x: int, radius_y: int):
+def _calculate_district(image: Image, district, count_pixels, pos_x: int, pos_y: int, radius_x: int, radius_y: int):
     for i in range(pos_y - radius_y, pos_y + radius_y + 1):
-        district = [x - y + z for x, y, z in zip(district,
-                                                 image[i, pos_x - radius_x],
-                                                 image[i, pos_x + radius_x + 1])]
-    return district
+        if i < 0 or i >= image.height:
+            continue
+        if pos_x - radius_x < 0:
+            district = [x + y for x, y in zip(district, image[i, pos_x + radius_x + 1])]
+            count_pixels += 1
+        elif pos_x + radius_x + 1 >= image.width:
+            district = [x - y for x, y in zip(district, image[i, pos_x - radius_x])]
+            count_pixels -= 1
+        else:
+            district = [x - y + z for x, y, z in zip(district,
+                                                     image[i, pos_x - radius_x],
+                                                     image[i, pos_x + radius_x + 1])]
+    return district, count_pixels
 
 
-def _calculate_base_district(image: Image, district, pos_y: int,  district_width: int, radius_y: int):
-    for j in range(district_width):
-        district = [x - y + z for x, y, z in zip(district,
-                                                 image[pos_y - radius_y, j],
-                                                 image[pos_y + radius_y + 1, j])]
-    return district
+def _calculate_base_district(image: Image, district, count_pixels, pos_y: int,  radius_x: int, radius_y: int):
+    for j in range(2 * radius_x + 1):
+        if pos_y - radius_y < 0:
+            district = [x + y for x, y in zip(district, image[pos_y + radius_y + 1, j])]
+            count_pixels += 1
+        elif pos_y + radius_y + 1 >= image.height:
+            district = [x - y for x, y in zip(district, image[pos_y - radius_y, j])]
+            count_pixels -= 1
+        else:
+            district = [x - y + z for x, y, z in zip(district,
+                                                     image[pos_y - radius_y, j],
+                                                     image[pos_y + radius_y + 1, j])]
+    return district, count_pixels
