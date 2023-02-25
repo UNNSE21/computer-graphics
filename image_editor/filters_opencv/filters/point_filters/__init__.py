cd6133f5e9ae0235e1e2555dd5fe428800c6e802
Luigi_Capello <Stepa.kashin.2018@gmail.com>
2023-02-25 18:18:11 +0300

    fix path to filters

--- image_editor/filters_opencv/filters/point_filters/__init__.py
+++ image_editor/filters_opencv/filters/point_filters/__init__.py
@@ -1,8 +1,7 @@
-from filters_opencv.filters.point_filters.inversion import invert
-from filters_opencv.filters.point_filters.mirror_image import horizontally, vertically
-from filters_opencv.filters.point_filters.scale import scale
-from filters_opencv.filters.point_filters.gray_scale import gray_scale
-from filters_opencv.filters.point_filters.sepia import sepia
-from filters_opencv.filters.point_filters.changing_brightness import change_brightness
-from filters_opencv.filters.point_filters.relocation import relocate
-from filters_opencv.filters.point_filters.rotation import rotate
+from filters_opencv.filters.point_filters.inversion.inversion import invert
+from filters_opencv.filters.point_filters.mirror_image.mirror_image import horizontally, vertically
+from filters_opencv.filters.point_filters.scale.scale import scale
+from filters_opencv.filters.point_filters.gray_scale.gray_scale import gray_scale
+from filters_opencv.filters.point_filters.sepia.sepia import sepia
+from filters_opencv.filters.point_filters.changing_brightness.changing_brightness import change_brightness
+from filters_opencv.filters.point_filters.relocation_&_rotation.relocation import relocate,rotate
